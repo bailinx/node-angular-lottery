138847dda0109bd3d0ff54e54d670d26e31ab6de
radishj <423261989@qq.com>
2016-02-03 17:01:32 +0800

    gulp require package

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp           = require('gulp'),
     minifycss      = require('gulp-minify-css'),
     rename         = require('gulp-rename'),
     // 为服务器特别定制的，快速、灵活、实施精益(lean implementation)的jQuery核心
-    cheerio        = require('gulp-cheerio');
+    cheerio        = require('gulp-cheerio'),
+    rjs            = require('gulp-requirejs');
 
 var path = {
     public   : "public/",
@@ -12,7 +13,7 @@ var path = {
     js       : "public/js/",
     less     : "public/less/",
     img      : "public/img/",
-    dist     : "dist/"
+    dist     : "public/dist/"
 }, env = {
     production: false
 };
@@ -34,6 +35,64 @@ gulp.task('replace', function() {
                 .pipe(gulp.dest(path.dist));
 });
 
+gulp.task('scripts', function() {
+    rjs({
+        //appDir: './public/js',
+        baseUrl: "./public/js",
+        removeCombined: true,
+        // 不能直接混淆，ng会注入失败
+        optimize: "none",
+        paths: {
+            'domReady': "../libs/requirejs-domready/domReady",
+            'angular': "../libs/angular/angular",
+            'uiRouter': "../libs/angular-ui-router/release/angular-ui-router",
+            'uiBootstrap': "../libs/angular-bootstrap/ui-bootstrap-tpls",
+            'angularAnimate': "../libs/angular-animate/angular-animate",
+            'angularToastr': "../libs/angular-toastr/dist/angular-toastr.tpls",
+            'angularStorage': "../libs/angular-local-storage/dist/angular-local-storage",
+            'ngFileUpload': "../libs/ng-file-upload/ng-file-upload-all",
+            'socket.io': "../libs/socket.io-client/socket.io",
+            'btford.socket-io': '../libs/angular-socket-io/socket',
+            'jquery': "../libs/jquery/dist/jquery",
+            'snow': "../plug/snow/snow"
+        },
+        shim: {
+            'angular': {
+                'deps': ['jquery'],
+                'exports': 'angular'
+            },
+            'btford.socket-io': {
+                'deps': ['angular', 'socket.io']
+            },
+            'uiRouter': {
+                deps: ['angular']
+            },
+            'uiBootstrap': {
+                deps: ['angular']
+            },
+            'ngFileUpload': {
+                deps: ['angular']
+            },
+            'angularAnimate': {
+                deps: ['angular']
+            },
+            'angularToastr': {
+                deps: ['angular']
+            },
+            'angularStorage': {
+                deps: ['angular']
+            }
+        },
+        name: "bootstrap",
+        out: "build.js"
+        //dir: "./public/build"
+    }, function(buildResponse){
+        console.log('build response', buildResponse);
+    })
+    .pipe(gulp.dest(path.dist));
+        //.pipe(concat("index.js"))
+        //.pipe(rename("index.min.js"))
+});
 // 编译less
 gulp.task('css', function() {
     if(env.production) {
@@ -62,4 +121,5 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['watch']);
+gulp.task('script', ['scripts']);
 gulp.task('build', ['set-production', 'css', 'replace']);
